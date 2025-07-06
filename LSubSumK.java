5c3f4b557f05ee25b131c15ce38cf7d0887305e4
Hashir2209 <114394925+Hashir2209@users.noreply.github.com>
2025-07-06 23:46:10 +0530

    Update LSubSumK.java

--- LSubSumK.java
+++ LSubSumK.java
@@ -4,6 +4,7 @@ public class LSubSumK {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
+        //Taking input
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
